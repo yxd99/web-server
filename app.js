73f82a9786ff315294bccb0648266b0236db02a8
Yexid Hernandez <yexid.9901@gmail.com>
2021-05-06 10:04:01 -0500

    dotenv added, files "generic, elements" with yours router ready.

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+require('dotenv').config();
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
+
+const basics = {
+  title: 'Curso node',
+  author: 'Yesid Hernandez',
+};
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    ...basics,
+  });
+});
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    ...basics,
+  });
+});
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    ...basics,
+  });
 });
 
 app.get('*', (req, res) => {
